5ecff6d3abff4a020c915e208e02d83cd41573b0
beatriz-camilo <abeatrizcamilo@gmail.com>
2026-05-17 21:54:45 -0300

    test: adiciona configuracao do vitest e remove definicao redundante de esbuild

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  esbuild: {
-    jsx: 'automatic',
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
